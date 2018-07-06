4403c89fe25a48d92bdfee6fe639cd24feaa1204
jiaxiaole2009 <32797796+jiaxiaole2009@users.noreply.github.com>
2018-07-07 00:55:46 +0800

    zhuanhan

--- ConsoleApplication1/类型转换显式加隐式/Program.cs
+++ ConsoleApplication1/类型转换显式加隐式/Program.cs
@@ -10,6 +10,44 @@ namespace 类型转换显式加隐式
     {
         static void Main(string[] args)
         {
+            LimitedInt li = 500;
+            int value = li;
+            Console.WriteLine("li :{0},value :{1}",li.TheValue,value);
         }
+    }
+    class LimitedInt
+    {
+        const int MaxValue = 100;
+        const int MinValue = 0;
+
+        public static implicit operator int(LimitedInt li)
+        {
+            return li.TheValue;
+        }
+
+        public static implicit operator LimitedInt(int x)
+        {
+            LimitedInt li = new LimitedInt();
+            li.TheValue = x;
+            return li;
+        }
+        private int _theValue = 0;
+        public int TheValue
+        {
+            get { return _theValue; }
+            set
+            {
+                if(value<MinValue)
+                {
+                    _theValue = 0;
+
+                }
+                else
+                {
+                    _theValue = value > MaxValue ? MaxValue : value;
+                }
+            }
+        }
+
     }
 }
