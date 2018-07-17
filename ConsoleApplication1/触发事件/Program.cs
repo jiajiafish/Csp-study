14d281ae7a56b1a7819efd770f508bc9f974e7b1
jiaxiaole2009 <32797796+jiaxiaole2009@users.noreply.github.com>
2018-07-17 18:45:45 +0800

    111

---
+++ ConsoleApplication1/触发事件/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 触发事件
+{
+    delegate void Handler();
+    //声明委托
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Incrementer incrementer = new Incrementer();
+            Dozens dozensCounter = new Dozens(incrementer);
+            incrementer.DoCount();
+            Console.WriteLine("number of dozens ={0}",dozensCounter.DozensCount);
+            Console.ReadKey();
+
+        }
+
+    }
+    class Incrementer
+    {
+        public event Handler CountedADozen;
+        public void DoCount()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i%12 == 0 && CountedADozen!=null)
+                {
+                    CountedADozen();
+                }
+            }
+        }
+    }
+    //上面的是发布者
+
+    class Dozens
+    {
+        public int DozensCount { get; private set; }
+        //设置成从外面只能get的属性
+        public Dozens(Incrementer incrementer)
+        {
+            DozensCount = 0;
+            incrementer.CountedADozen += Incrementer_CountedADozen;
+        }
+
+        private void Incrementer_CountedADozen()
+        {
+            DozensCount++;
+        }
+    }
+
+
+
+
+    
+}
